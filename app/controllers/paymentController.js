bc6f469f57896ef1446a1798dd50402c615d9237
mohamadali <ma.khajeian@gmail.com>
2022-12-02 18:45:17 +0330

    modifed

--- app/controllers/paymentController.js
+++ app/controllers/paymentController.js
@@ -1,4 +1,5 @@
 const ZarinpalCheckout = require('zarinpal-checkout');
+// Merchant ID placed here
 const zarinpal = ZarinpalCheckout.create('34940947-178e-4e8c-ab1b-5bfdbb363c03', false);
 const User = require('../models/userModel.js');
 const UserCharge = require('../models/userChargeModel.js');
@@ -8,10 +9,12 @@ const persianDate = require('persian-date');
 exports.AddPayment = function(req, res) {
     zarinpal.PaymentRequest({
         Amount: req.query.amount,
-        CallbackURL: 'http://daneshpark.ir:1090/payment/callback?token=' + req.query.token + "&amount=" + req.query.amount + "&description=" + req.query.description,
+        CallbackURL: 'http://daneshpark.ir:1090/payment/callback?token=' +
+        req.query.token + "&amount=" + req.query.amount + "&description=" 
+        + req.query.description,
         Description: req.query.description,
-        Email: 'ma.khajeian@gmail.com',
-        Mobile: '09026616530'
+        Email: 'school@daneshpark.org',
+        Mobile: '09177131738'
     }).then(response => {
         if (response.status === 100) {
             return res.json({CallbackURL: response.url});
@@ -40,17 +43,21 @@ exports.CallBackPayment = function(req, res) {
           if (response.status !== 100) {
             console.log('NOT Verified!');
           } else {
-            var userCharge = new UserCharge({ref_id: response.RefID, authority: req.query.Authority, status: req.query.Status, user_id: user._id, amount: req.query.amount/1000, description: req.query.description, date: new persianDate(new Date((new Date()).toLocaleString("en-US", {timeZone: "Asia/Tehran"}))).format("LLLL")}); 
+            var userCharge = new UserCharge({ref_id: response.RefID, 
+              authority: req.query.Authority, status: req.query.Status, 
+              user_id: user._id, amount: req.query.amount/1000,
+              description: req.query.description,
+              date: new persianDate(new Date((new Date()).toLocaleString("en-US", {timeZone: "Asia/Tehran"}))).format("LLLL")}); 
             userCharge.save(function(err, res){
               if(err) throw(err);
             });
-            return res.json({status: response.status});
+            return res.json({status: req.query.Status});
           }
         }).catch(err => {
           console.error(err);
         });
       });  
     } else if (req.query.Status == "NOK"){
-      //...    
+      return res.json({status: req.query.Status});  
     }
 };
